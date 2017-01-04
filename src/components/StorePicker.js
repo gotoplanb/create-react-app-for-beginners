04ed458e99ddfd05eae4949df8daeaaf4f41d03c
Dave Stanton <davestanton.us@gmail.com>
2017-01-04 16:13:08 -0500

    End 4 of 30. Basic JSX.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    // Vanilla JavaScript comment
+    return (
+      <form className="store-selector">
+        { /* JSX comment must be inside your returned element */ }
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
   }
 }
 
