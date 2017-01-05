c1d5d1849c616d0609915b224485d5ded3f2152d
Dave Stanton <davestanton.us@gmail.com>
2017-01-05 18:32:46 -0500

    WIP Does not transitionTo.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -12,8 +12,10 @@ class StorePicker extends React.Component {
     event.preventDefault();
     console.log('You Changed the URL');
     // First grab the text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
     // Second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
@@ -27,6 +29,10 @@ class StorePicker extends React.Component {
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
