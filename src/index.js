c04ac16ebe6705cb9b8034c1af1214e70f896f57
Dave Stanton <davestanton.us@gmail.com>
2017-01-05 13:26:06 -0500

    End 9 of 30.
    Using ReactRouter docs for the implementation.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
+import { Router, Route, Link, browserHistory } from 'react-router'
 
+import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+), document.getElementById('root'))
