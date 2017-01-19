badeaa450f4435fc9b19a54da110dc61f52431fc
Dave Stanton <davestanton.us@gmail.com>
2017-01-19 17:42:51 -0500

    End 16 of 30.
    Passed down the key value explicitly via props as “index”.

--- src/components/App.js
+++ src/components/App.js
@@ -54,8 +54,10 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key}
-                  details={this.state.fishes[key]} /> )
+                .map(key => <Fish
+                  key={key}
+                  index={key}
+                  details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
