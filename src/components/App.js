f0304f956b47fbd163613c8b81a65c485cf1d6a4
Dave Stanton <davestanton.us@gmail.com>
2017-01-19 15:49:29 -0500

    Use props to make the method available elsewhere.

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
