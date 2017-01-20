db2f5e21c2a9226126557bf633928cb6ae06c8ac
Dave Stanton <davestanton.us@gmail.com>
2017-01-20 11:00:42 -0500

    Handling changes to any of our fields.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,9 +3,35 @@ import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
 
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+    // take a copy of the fish
+    const updatedFish = {...fish,
+      [e.target.name]: e.target.value
+    }
+    console.log(updatedFish);
+  }
+
   renderInventory(key) {
+    const fish = this.props.fishes[key];
     return (
-      <p>{key}</p>
+      <div className="fish-edit" key={key}>
+        <input type="text" name="name" defaultValue={fish.name} onChange={(e) => this.handleChange(e, key)} />
+        <input type="text" name="price" defaultValue={fish.price} onChange={(e) => this.handleChange(e, key)} />
+        <select type="text" name="status" defaultValue={fish.status} onChange={(e) => this.handleChange(e, key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" name="desc" defaultValue={fish.desc} onChange={(e) => this.handleChange(e, key)}>
+        </textarea>
+        <input type="text" name="image" defaultValue={fish.image} onChange={(e) => this.handleChange(e, key)} />
+      </div>
     )
   }
 
