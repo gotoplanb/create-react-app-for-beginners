4d7683d463c7ee1751e39bc7e6039a87eeb41def
Dave Stanton <davestanton.us@gmail.com>
2017-01-23 15:29:53 -0500

    Refactored remaining of .bind() to use Property Initializers.

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,6 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.addFish = this.addFish.bind(this);
-    this.updateFish = this.updateFish.bind(this);
-    // this.removeFish = this.removeFish.bind(this);
-    // this.loadSamples = this.loadSamples.bind(this);
-    this.addToOrder = this.addToOrder.bind(this);
-    this.removeFromOrder = this.removeFromOrder.bind(this);
     // Set initial state
     this.state = {
       fishes: {},
@@ -51,7 +45,7 @@ class App extends React.Component {
     localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
 
-  addFish(fish) {
+  addFish = (fish) => {
     // First copy your state
     const fishes = {...this.state.fishes};
     // Next add in the new fish
@@ -59,13 +53,13 @@ class App extends React.Component {
     fishes[`fish-${timestamp}`] = fish;
     // Finally update your state
     this.setState({ fishes });
-  }
+  };
 
-  updateFish(key, updatedFish) {
+  updateFish = (key, updatedFish) => {
     const fishes = {...this.state.fishes};
     fishes[key] = updatedFish;
     this.setState({ fishes });
-  }
+  };
 
   removeFish = (key) => {
     const fishes = {...this.state.fishes};
@@ -79,21 +73,21 @@ class App extends React.Component {
     });
   };
 
-  addToOrder(key) {
+  addToOrder = (key) => {
     // First copy your state
     const order = {...this.state.order};
     // Increment an existing order item or set to 1
     order[key] = order[key] + 1 || 1;
     // Update state
     this.setState({ order });
-  }
+  };
 
-  removeFromOrder(key) {
+  removeFromOrder = (key) => {
     const order = {...this.state.order};
     // We can use delete since we don't have to worry about Firebase
     delete order[key];
     this.setState({ order });
-  }
+  };
 
   render() {
     return (
